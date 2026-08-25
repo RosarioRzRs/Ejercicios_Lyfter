453c70dc542db72b2eda9ee1f8c295868499791c
Rosario Ramirez <rosario_rzrs@hotmail.com>
2026-08-24 21:44:46 -0600

    The requested changes were made: validation is done in the __init__, the object creation is placed inside the try

--- 4 Python Intermedio/1 OOP/Ejercicios_Extra/1_area_perimetro.py
+++ 4 Python Intermedio/1 OOP/Ejercicios_Extra/1_area_perimetro.py
@@ -1,5 +1,8 @@
 class Rectangle:
     def __init__(self, width, height):
+        if width < 0 or height < 0:
+            raise ValueError()
+        
         self.width = width
         self.height = height
 
@@ -14,23 +17,21 @@ class Rectangle:
 def request_measures():
     width = int(input("Ingrese la  altura: "))
     height = int(input("Ingrese el ancho: "))
-
+    
     return width, height
 
 def main():
     width, height = request_measures()
-    rectangle = Rectangle(width, height)
     try:   
-        if width < 0 or height < 0:
-            raise ValueError()
-
+        rectangle = Rectangle(width, height)
         print(rectangle.get_area())
         print(rectangle.get_perimeter())
     except ValueError as ex:
         print("Existe un valor negativo, los valores deben ser positivos")
-        # raise ex
+       
+        
+       
     
-  
 
 main()
 
